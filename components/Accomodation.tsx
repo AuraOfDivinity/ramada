f419920ab64c8ff0dc29e7ea041593ecefe73c3a
Asel <asel.peiris@arimaclanka.com>
2024-02-20 01:44:37 +0530

    Accomodation

---
+++ components/Accomodation.tsx
@@ -0,0 +1,74 @@
+import { SUITES_LIST } from "@/constants";
+import Image from "next/image";
+
+const Accomodation = () => {
+  return (
+    <section className="flexCenter py-20">
+      <div className="flex max-container padding-container relative w-full">
+        {/* extract into single styles */}
+        <div className="flex flex-col flex-1">
+          <div className="font-primary regular-26 text-red-primary pb-2">
+            Accomodation
+          </div>
+          <div className="font-primary bold-40 text-gray-primary">
+            Comfortable rooms just for you
+          </div>
+          <ul className="grid md:grid-cols-4 gap-x-7">
+            {SUITES_LIST.map((suite) => (
+              <SuiteCard
+                suiteName={suite.name}
+                features={suite.features}
+                imageUrl={suite.imageUrl}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+type SuiteCardProps = {
+  suiteName: string;
+  imageUrl: string;
+  features: string[];
+};
+const SuiteCard = ({ suiteName, imageUrl, features }: SuiteCardProps) => {
+  return (
+    <div className=" rounded-md overflow-hidden shadow-lg">
+      <Image
+        className="w-full p-3 rounded-2xl max-h-[200px]"
+        src={imageUrl}
+        alt="suite image"
+        width={250}
+        height={180}
+      />
+      <div>
+        <div className="font-primary bold-22 text-black-primary mb-2 text-center">
+          {suiteName}
+        </div>
+      </div>
+      <ul className="list-disc ml-7 my-1">
+        {features.map((feature) => (
+          <li className="regular-16 font-secondary text-black-primary">
+            {feature}
+          </li>
+        ))}
+      </ul>
+      <div className="p-3">
+        <button className="flexCenter rounded-md bg-white w-full h-[40px] border-[1px] border-red-primary mb-1">
+          <label className="whitespace-nowrap text-red-primary font-primary regular-18">
+            Explore
+          </label>
+        </button>
+        <button className="flexCenter rounded-md bg-red-primary w-full h-[40px]">
+          <label className="whitespace-nowrap text-white font-primary regular-18">
+            Book Now
+          </label>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Accomodation;
--- constants/index.ts
+++ constants/index.ts
@@ -7,10 +7,56 @@ export const NAV_LINKS = [
   { href: "/", key: "delivery", label: "Delivery" },
 ];
 
+// HERO SECTION
 export const heroHeadingText =
   "Lorem Ipsum is\nsimply dummy text of the\nprinting";
 export const heroSubText =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem\nIpsum has been the industry's standard dummy text ever since the 1500s, when an\nunknown ";
 
+// ABOUT US
 export const aboutUsDescription =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown .Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,";
+
+// ACCOMODATION SECTION
+export const SUITES_LIST = [
+  {
+    name: "Twin Bedded Deluxe Room",
+    imageUrl: "/twinbedroom.jpeg",
+    features: [
+      "Lorem ipsum",
+      "Typesetting industry",
+      "Lorem ipsum has been",
+      "The industry standards",
+    ],
+  },
+  {
+    name: "King Bed Deluxe Room",
+    imageUrl: "/kingdeluxe.jpeg",
+    features: [
+      "Lorem ipsum",
+      "Typesetting industry",
+      "Lorem ipsum has been",
+      "The industry standards",
+    ],
+  },
+  {
+    name: "Splendour Suite",
+    imageUrl: "/splendour.jpeg",
+    features: [
+      "Lorem ipsum",
+      "Typesetting industry",
+      "Lorem ipsum has been",
+      "The industry standards",
+    ],
+  },
+  {
+    name: "Premier Suite",
+    imageUrl: "/premier.jpeg",
+    features: [
+      "Lorem ipsum",
+      "Typesetting industry",
+      "Lorem ipsum has been",
+      "The industry standards",
+    ],
+  },
+];
