4fddf8a794c6b833d28c44994c91af27221ecb34
Asel <asel.peiris@arimaclanka.com>
2024-02-19 23:24:31 +0530

    Minor fix

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -24,7 +24,7 @@ const AboutUs = () => {
             </div>
           </div>
         </div>
-        /** TODO: Extract to single style */
+        {/* TODO: Extract to single style */}
         <div className="flex-1">
           <div className="flex justify-end">
             <div
