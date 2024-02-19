8a51188bcf6756544368f005a4481373aa567418
Asel <asel.peiris@arimaclanka.com>
2024-02-19 23:36:48 +0530

    Adding container padding to hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,14 +4,15 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <section className="flex flex-col">
-      <div className="flexCenter max-container relative w-full overflow-hidden">
+      <div className="flexCenter max-container padding-container relative w-full overflow-hidden">
         <Image
           src="/hero.jpeg"
           alt="hero"
-          width={1440}
-          height={580}
+          width={0}
+          height={0}
+          sizes="100vw"
           className=" brightness-50 object-cover object-top 2xl:rounded-5xl"
-          style={{ maxHeight: "700px" }}
+          style={{ maxHeight: "700px", width: "100%", height: "auto" }}
         />
       </div>
 
